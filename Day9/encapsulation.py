6d089f8eeb8bc96bce4d88417deb0d0d2127d22c
ArjunMS07 <arj.manimalayil@gmail.com>
2026-04-17 10:36:40 +0530

    Added Day9 tasks

---
+++ Day9/encapsulation.py
@@ -0,0 +1,54 @@
+# Student Class
+class Student:
+    name = "ABC"   # class variable
+
+    def __init__(self, name, id_number):
+        self.name = name              # public
+        self.__id_number = id_number  # private
+
+    # Getter
+    def get_id(self):
+        return self.__id_number
+
+    # Setter
+    def set_id(self, new_id):
+        if len(str(new_id)) == 4:
+            self.__id_number = new_id
+            print("ID updated successfully")
+        else:
+            print("Invalid ID (must be 4 digits)")
+
+    # Instance method
+    def study(self, subject):
+        print(f"{self.name} is studying {subject}")
+
+    # Class method
+    @classmethod
+    def school_info(cls):
+        print(f"School Name: {cls.name}")
+
+
+# Create object
+student1 = Student("Arjun", 1234)
+
+# Access public attribute
+print("Name:", student1.name)
+
+# Try accessing private attribute (will cause error if uncommented)
+# print(student1.__id_number)
+
+# Access private using getter
+print("ID (using getter):", student1.get_id())
+
+# Update ID using setter
+student1.set_id(5678)
+print("Updated ID:", student1.get_id())
+
+# Invalid update
+student1.set_id(12)
+
+# Instance method
+student1.study("Python")
+
+# Class method
+Student.school_info()
